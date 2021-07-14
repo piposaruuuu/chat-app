3473ad9e1ae15fa53e12606205b60da3a63c07a5
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-07-14 17:22:33 +0900

    Active Storageの導入

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -26,5 +26,10 @@ class RoomsController < ApplicationController
   def room_params
    params.require(:room).permit(:name, user_ids: [])
   end
+  
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+    
 end
 
